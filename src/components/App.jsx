761467c00e398d9f6b2baa14f251043ae835acfb
Olga Boiko <Itnumber1@ukr.net>
2023-07-23 14:13:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ import { Section, H2 } from "./App.style";
 class App extends Component { 
   state = {
     contacts: [
-    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
     filter: ''
   }
@@ -54,10 +54,12 @@ if(this.state.contacts!==prevState.contacts){
   console.log('contacts обновились')
   localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
 }}  
+
 componentDidMount(){
   const contacts= localStorage.getItem('contacts')
   const contactsParse=JSON.parse(contacts)
-  this.setState({contacts: contactsParse});
+  if(contactsParse){this.setState({contacts: contactsParse});}
+  
 }
  
       render() {
